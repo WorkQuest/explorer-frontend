11f62d40b805b00f57ad0aaa90c5c2df2784d73b
ghost1 <sonofagronom@gmail.com>
2022-01-24 17:14:53 +0700

    try redirects

--- middleware/redirects.js
+++ middleware/redirects.js
@@ -1,8 +1,8 @@
 export default function (req, res, next) {
   const redirects = [
     { from: '/', to: '/home' },
-    { from: 'home/blocks', to: '/blocks' },
-    { from: 'home/transactions', to: '/transactions' },
+    { from: '/home/blocks', to: '/blocks' },
+    { from: '/home/transactions', to: '/transactions' },
   ];
   const redirect = redirects.find((r) => r.from === req.url);
   if (redirect) {
