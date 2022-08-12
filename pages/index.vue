2c6d168795ad7233e626357a8f6f96be91c789b0
SergeyKovalchuk2086 <kovalchuk.s.v@inbox.ru>
2022-08-12 15:06:29 +0700

    fix: fix bags from testers

--- pages/index.vue
+++ pages/index.vue
@@ -203,7 +203,7 @@ export default Vue.extend({
             alt: 'icon market-cap',
           },
           text: this.$t('ui.statistics.marketCap'),
-          value: `$${this.FormattingLargeNumber(this.calcMarketCap)}`,
+          value: `$${(this.FormattingLargeNumber(this.calcMarketCap)).toFixed(2)}`,
         },
         {
           class: 'gas',
@@ -355,7 +355,7 @@ export default Vue.extend({
 :deep(.google-visualization-tooltip) {
   border-radius: 8px;
   border: 1px solid #F7F8FA;
-  width: 130px!important;
+  width: 140px!important;
   height: 45px!important;
 }
 :deep(.google-visualization-tooltip-item span) {
